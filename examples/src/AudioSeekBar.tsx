dc75292ea7752ead82eb8b56eb83d8af63153c37
Erich Kuerschner <erichk@vailsys.com>
2020-08-16 14:11:15 -0500

    WIP

--- examples/src/AudioSeekBar.tsx
+++ examples/src/AudioSeekBar.tsx
@@ -6,7 +6,11 @@ import React, {
     FunctionComponent,
     MouseEvent
 } from "react"
-import { useAudioPlayer, useAudioPosition } from "react-use-audio-player"
+import {
+    useAudioPlayer,
+    useAudioPosition,
+    useAudioEvents
+} from "react-use-audio-player"
 import "./AudioSeekBar.scss"
 
 interface AudioSeekBarProps {
@@ -18,6 +22,34 @@ export const AudioSeekBar: FunctionComponent<AudioSeekBarProps> = props => {
     const { position, duration, seek } = useAudioPosition({
         highRefreshRate: true
     })
+
+    const registerEvents = useAudioEvents()
+
+    const events1 = [
+        { position: 0, name: "event1" },
+        { position: 2, name: "event2" },
+        { position: 3, name: "event3" }
+    ]
+
+    const events2 = [
+        { position: 1, name: "event21" },
+        { position: 3, name: "event22" },
+        { position: 4, name: "event23" }
+    ]
+    registerEvents({
+        events: events1,
+        callback: event => {
+            console.log("Event type 1!", event)
+        }
+    })
+
+    registerEvents({
+        events: events2,
+        callback: event => {
+            console.log("Event type 2!", event)
+        }
+    })
+
     const { playing } = useAudioPlayer()
     const [barWidth, setBarWidth] = useState("0%")
 
